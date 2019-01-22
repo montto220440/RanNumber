d7889755bcc5409f938a5b589a2352f602ceb69d
Admin <admin@MAC23.local>
2019-01-22 09:52:34 +0700

    Initial Commit

---
+++ NumRandom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NumRandom
+//
+//  Created by Admin on 22/1/2562 BE.
+//  Copyright © 2562 montto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
